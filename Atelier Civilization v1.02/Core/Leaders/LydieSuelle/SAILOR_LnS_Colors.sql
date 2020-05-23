9824c420b50f0065baf8a16e68e12115cf8142f2
ActualSailorCat <52355344+ActualSailorCat@users.noreply.github.com>
2020-05-23 11:24:47 -0400

    Updated Puni unit portrait.

---
+++ Atelier Civilization v1.02/Core/Leaders/LydieSuelle/SAILOR_LnS_Colors.sql
@@ -0,0 +1,14 @@
+-----------------------------------------------
+-- PlayerColors
+-----------------------------------------------
+INSERT INTO    PlayerColors (Type, Usage, PrimaryColor, SecondaryColor, Alt1PrimaryColor, Alt1SecondaryColor, Alt2PrimaryColor, Alt2SecondaryColor, Alt3PrimaryColor, Alt3SecondaryColor)
+VALUES	('LEADER_SAILOR_LNS',
+			'Unique',
+			'COLOR_SAILOR_ATLEER_B_PRIMARY',
+			'COLOR_SAILOR_ATLEER_B_SECONDARY',
+			'COLOR_SAILOR_ATLEER_A_PRIMARY',
+			'COLOR_SAILOR_ATLEER_A_SECONDARY',
+			'COLOR_SAILOR_ATLEER_C_PRIMARY',
+			'COLOR_SAILOR_ATLEER_C_SECONDARY',
+			'COLOR_SAILOR_ATLEER_D_PRIMARY',
+			'COLOR_SAILOR_ATLEER_D_SECONDARY');
