e7bb7ebe9ebdab5581ff16fc18708e36fd0390ce
ActualSailorCat <52355344+ActualSailorCat@users.noreply.github.com>
2020-06-13 15:52:57 -0400

    v1.22

---
+++ Atelier Civilization v1.22/Core/Civilization/SAILOR_ATLEER_XP.sql
@@ -0,0 +1,10 @@
+--=============================================
+-- Atleer Civ / Rise & Fall and Gathering Storm
+--=============================================
+-----------------------------------------------
+-- MomentIllustrations
+-----------------------------------------------
+INSERT INTO MomentIllustrations
+		(MomentIllustrationType,					MomentDataType,				GameDataType,					Texture)
+VALUES	('MOMENT_ILLUSTRATION_UNIQUE_UNIT',			'MOMENT_DATA_UNIT',			'UNIT_SAILOR_ATLEER_UU',		'Moment_UniqueUnit_SAILOR_Atleer_UU.dds'),
+		('MOMENT_ILLUSTRATION_UNIQUE_IMPROVEMENT',	'MOMENT_DATA_IMPROVEMENT',	'IMPROVEMENT_SAILOR_ATLEER_UI',	'Moment_Infrastructure_SAILOR_Atleer_UI.dds');
